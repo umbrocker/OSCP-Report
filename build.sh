b3a427c6ce37c01658f56713b5e1615777ea4a33
Gipsz Jakab <jakab@gipsz.com>
2025-07-02 09:50:43 +0200

    added build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pandoc $1 -o $2.pdf \
+--from markdown+yaml_metadata_block+raw_html \
+--template eisvogel \
+--table-of-contents \
+--toc-depth 6 \
+--number-sections \
+--top-level-division=chapter \
+--highlight-style breezedark \
+--resource-path=.:/home/kali/report
